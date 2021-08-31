a4fd3f2a7f6ef31cf2a354a940ed295ba37cc699
lateef10 <lateefrasheed10@gmail.com>
2021-08-30 22:29:45 -0400

    API Gateway yaml configuration

--- InfShop.Web/Services/BaseService.cs
+++ InfShop.Web/Services/BaseService.cs
@@ -10,6 +10,9 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
 
 namespace InfShop.Web.Services
 {
@@ -24,66 +27,67 @@ namespace InfShop.Web.Services
             this.httpClient = httpClient;
         }
 
-        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+    public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+    {
+        try
         {
-            try
+
+            var client = httpClient.CreateClient(Constants.CleanArchAPI);
+            HttpRequestMessage message = new HttpRequestMessage();
+            message.Headers.Add("Accept", "application/json");
+            message.RequestUri = new Uri(apiRequest.Url);
+            client.DefaultRequestHeaders.Clear();
+            if (apiRequest.Data != null)
             {
-                var client = httpClient.CreateClient(Constants.CleanArchAPI);
-                HttpRequestMessage message = new HttpRequestMessage();
-                message.Headers.Add("Accept", "application/json");
-                message.RequestUri = new Uri(apiRequest.Url);
-                client.DefaultRequestHeaders.Clear();
-                if (apiRequest.Data != null)
-                {
-                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
-                        Encoding.UTF8, "application/json");
-                }
-
-                if (!string.IsNullOrEmpty(apiRequest.AccessToken))
-                {
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiRequest.AccessToken);
-                }
-
-                HttpResponseMessage apiResponse = null;
-                switch (apiRequest.ApiType)
-                {
-                    case SD.ApiType.POST:
-                        message.Method = HttpMethod.Post;
-                        break;
-                    case SD.ApiType.PUT:
-                        message.Method = HttpMethod.Put;
-                        break;
-                    case SD.ApiType.DELETE:
-                        message.Method = HttpMethod.Delete;
-                        break;
-                    default:
-                        message.Method = HttpMethod.Get;
-                        break;
-                }
-                apiResponse = await client.SendAsync(message);
-
-                var apiContent = await apiResponse.Content.ReadAsStringAsync();
-                var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContent);
-                return apiResponseDto;
-
+                message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                    Encoding.UTF8, "application/json");
             }
-            catch (Exception e)
+
+            if (!string.IsNullOrEmpty(apiRequest.AccessToken))
             {
-                var dto = new ResponseDto
-                {
-                    DisplayMessage = "Error",
-                    ErrorMessages = new List<string> { Convert.ToString(e.Message) },
-                    IsSuccess = false
-                };
-                var res = JsonConvert.SerializeObject(dto);
-                var apiResponseDto = JsonConvert.DeserializeObject<T>(res);
-                return apiResponseDto;
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiRequest.AccessToken);
             }
+
+            HttpResponseMessage apiResponse = null;
+            switch (apiRequest.ApiType)
+            {
+                case SD.ApiType.POST:
+                    message.Method = HttpMethod.Post;
+                    break;
+                case SD.ApiType.PUT:
+                    message.Method = HttpMethod.Put;
+                    break;
+                case SD.ApiType.DELETE:
+                    message.Method = HttpMethod.Delete;
+                    break;
+                default:
+                    message.Method = HttpMethod.Get;
+                    break;
+            }
+            apiResponse = await client.SendAsync(message);
+
+            var apiContent = await apiResponse.Content.ReadAsStringAsync();
+            var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContent);
+            return apiResponseDto;
+
         }
-
-        public void Dispose()
+        catch (Exception e)
         {
-            GC.SuppressFinalize(true);
+            var dto = new ResponseDto
+            {
+                DisplayMessage = "Error",
+                ErrorMessages = new List<string> { Convert.ToString(e.Message) },
+                IsSuccess = false
+            };
+            var res = JsonConvert.SerializeObject(dto);
+            var apiResponseDto = JsonConvert.DeserializeObject<T>(res);
+            return apiResponseDto;
         }
     }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(true);
+    }
+}
 }

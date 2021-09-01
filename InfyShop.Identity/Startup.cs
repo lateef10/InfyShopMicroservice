c278eba435ee80c2faa72b0082255f732f3735f3
lateef10 <lateefrasheed10@gmail.com>
2021-09-01 12:08:51 -0400

    Working yaml on Azure Kubernetes

--- InfyShop.Identity/Startup.cs
+++ InfyShop.Identity/Startup.cs
@@ -79,6 +79,7 @@ namespace InfyShop.Identity
             builder.AddDeveloperSigningCredential(); //For Development Purpose, use AddSigningCredential() for PROD
 
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
